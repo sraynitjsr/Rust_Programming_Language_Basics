901e0c706eade6da8ed9c2d453b48eaafbcf50ff
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-04-11 23:57:03 +0530

    Arthmetic Operations in Rust

---
+++ math_operations.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let num1 = 10 ;
+       let num2 = 5;
+       let mut res:i64;
+    
+       res = num1 + num2;
+       println!("Sum => {} ",res);
+    
+       res = num1 - num2;
+       println!("Difference => {} ",res) ;
+    
+       res = num1*num2 ;
+       println!("Product => {} ",res) ;
+    
+       res = num1/num2 ;
+       println!("Quotient => {} ",res);
+    }
+    
