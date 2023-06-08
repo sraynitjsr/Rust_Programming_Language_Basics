7c57ff7e8a9998822cde8208aff15e7a535774c1
Subhradeep Ray <subhradeepray2017@gmail.com>
2023-06-08 15:59:44 +0530

    If Else Statements in RUST

---
+++ if_else.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let number = 10;
+
+    if number < 0 {
+        println!("The number is negative.");
+    } else if number == 0 {
+        println!("The number is zero.");
+    } else {
+        println!("The number is positive.");
+    }
+}
