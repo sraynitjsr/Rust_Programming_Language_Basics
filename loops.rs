235cc11e9f04fa58669242651b1c7f429893d4ab
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-09-29 16:45:09 +0530

    Loops in RUST

--- loops.rs
+++ loops.rs
@@ -1,1 +1,12 @@
+fn main() {
+    let mut count = 0;
 
+    loop {
+        println!("Count: {}", count);
+        count += 1;
+
+        if count == 5 {
+            break; // Exit the loop when count reaches 5
+        }
+    }
+}
