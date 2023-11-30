8a71ba7adcad85a9bb62590976ef3cea1adec184
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-11-30 23:48:24 +0530

    Multi Threading in RUST

--- threading_in_rust.rs
+++ threading_in_rust.rs
@@ -21,26 +21,3 @@ fn main() {
 
     println!("Main thread exiting.");
 }
-use std::thread;
-
-fn main() {
-    let handle1 = thread::spawn(|| {
-        for i in 1..=5 {
-            println!("Thread 1: Count {}", i);
-            thread::sleep_ms(1000);
-        }
-    });
-
-    let handle2 = thread::spawn(|| {
-        for i in 1..=5 {
-            println!("Thread 2: Count {}", i);
-            thread::sleep_ms(800);
-        }
-    });
-
-    // Wait for both threads to finish
-    handle1.join().unwrap();
-    handle2.join().unwrap();
-
-    println!("Main thread exiting.");
-}
