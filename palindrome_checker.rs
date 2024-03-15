83ed6c4d8e97c6a7d35bf5d1fd498ddf5cba2e39
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-03-15 18:14:49 +0530

    String and Number Both Palindrome Checking

--- palindrome_checker.rs
+++ palindrome_checker.rs
@@ -1,14 +1,27 @@
-fn is_palindrome(num: i32) -> bool {
+fn is_palindrome_num(num: i32) -> bool {
     let num_str = num.to_string();
     let reversed_str: String = num_str.chars().rev().collect();
     num_str == reversed_str
 }
 
+fn is_palindrome_str(input: &str) -> bool {
+    let input = input.to_lowercase();
+    let reversed_str: String = input.chars().rev().collect();
+    input == reversed_str
+}
+
 fn main() {
     let num = 12321;
-    if is_palindrome(num) {
-        println!("{} is a palindrome.", num);
+    if is_palindrome_num(num) {
+        println!("{} is a palindrome as a number.", num);
     } else {
-        println!("{} is not a palindrome.", num);
+        println!("{} is not a palindrome as a number.", num);
+    }
+
+    let word = "Deified";
+    if is_palindrome_str(word) {
+        println!("{} is a palindrome as a string.", word);
+    } else {
+        println!("{} is not a palindrome as a string.", word);
     }
 }
